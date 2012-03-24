60fced93072e5a22648310b2af3bf7eaddc84405
berson_r <berson_r@epita.fr>
2012-03-24 22:56:52 +0100

    Added the ability to print polynomial expressions (change the bool value at the top of the file).

--- approx.py
+++ approx.py
@@ -3,18 +3,22 @@ import functools
 
 SCREEN_WIDTH = 1640
 SCREEN_HEIGHT = 980
+DISPLAY_FORMULA = False
 
-
-def newton(points, x):
+def newton(points, formula, x):
   """
     Using Newton's method, interpol the points present in the 'points' list
       and return the value of the polynom at point x.
   """
 
+  if DISPLAY_FORMULA and not formula:
+    formula_get = True
+    formula.append('Newton :\n')
+  else:
+    formula_get = False
+
   res = 0.0
   array = []
-
-  points = sorted (points)
 
   for j in xrange(0, len(points)):
     array.append([0.0] * (len(points) + 1))
@@ -24,12 +28,23 @@ def newton(points, x):
   res += array[0][1]
   tmp = x - array[0][0]
 
+  if formula_get:
+    formula.append(str(res))
+    formula.append(' + ')
+    formula.append('(x - ' + str(array[0][0]) + ')')
+
   for i in xrange(2, len(points) + 1):
     for j in xrange(0, len(points) - i + 1):
       try:
-        array[j][i] = float(array[j + 1][i - 1] - array[j][i - 1]) / float(array[j + i - 1][0] - array[j][0])
+        array[j][i] = float(array[j + 1][i - 1] - array[j][i - 1])            \
+          / float(array[j + i - 1][0] - array[j][0])
       except:
         pass
+    if formula_get:
+      formula.append(' * ')
+      formula.append(str(array[0][i]))
+      formula.append(' + ')
+      formula.append(formula[-4] + '(x - ' + str(array[i - 1][0]) + ')')
     res += tmp * array[0][i]
     tmp *= x - array[i - 1][0]
 
@@ -37,11 +52,17 @@ def newton(points, x):
 
 
 
-def lagrange(points, x):
+def lagrange(points, formula, x):
   """
     Using Lagrange's method, interpol the points present in the 'points' list
     and return the value of the polynom at point x.
   """
+
+  if DISPLAY_FORMULA and not formula:
+    formula_get = True
+    formula.append('Lagrange :\n')
+  else:
+    formula_get = False
 
   res = 0.0
 
@@ -52,9 +73,16 @@ def lagrange(points, x):
         continue
       try:
         tmp *= float(x - points[j][0]) / float(points[i][0] - points[j][0])
+        if formula_get:
+          formula.append('(x - ' + str(points[j][0]) + ') / ('                \
+              + str(points[i][0]) + ' - ' + str(points[j][0]) + ')')
+          formula.append(' + ')
       except:
         pass
     res += tmp
+
+  if formula_get:
+    del formula[-1]
 
   return int(res)
 
@@ -67,11 +95,14 @@ def interpol(points, method):
   """
 
   result = []
-  points_compute = functools.partial(method, points)
+  formula = []
+  points_compute = functools.partial(method, points, formula)
 
   for i in xrange(0, SCREEN_WIDTH, 2):
     result.append((i, points_compute(i)))
 
+  if formula:
+    print ''.join(formula)
   return result
 
 
@@ -82,6 +113,9 @@ def main():
   running = 1
 
   points = []
+  changed = True
+  l1 = []
+  l2 = []
 
 
   while running:
@@ -91,19 +125,26 @@ def main():
       elif event.type == pygame.MOUSEBUTTONDOWN:
         x, y = pygame.mouse.get_pos()
         points.append((x, y))
+        changed = True
+        if len(points) > 1:
+          l1 = interpol(points, lagrange)
+          l2 = interpol(points, newton)
       elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
         running = 0
 
-    screen.fill((0, 0, 0))
-    pygame.draw.aaline(screen, (100, 0, 0), (0, SCREEN_HEIGHT / 2), (SCREEN_WIDTH, SCREEN_HEIGHT / 2), 1)
-    pygame.draw.aaline(screen, (100, 0, 0), (SCREEN_WIDTH / 2, 0), (SCREEN_WIDTH / 2, SCREEN_HEIGHT), 1)
-    for p in points:
-      rect = pygame.Rect(p[0] - 5, p[1] - 5, 10, 10)
-      pygame.draw.rect(screen, (0, 0, 255), rect, 4)
-    if len(points) > 1:
-      pygame.draw.aalines(screen, (0, 255, 0), False, interpol(points, lagrange), 1)
-      pygame.draw.aalines(screen, (255, 0, 0), False, interpol(points, newton), 1)
-    pygame.display.flip()
+    if changed:
+
+      screen.fill((0, 0, 0))
+      pygame.draw.aaline(screen, (100, 0, 0), (0, SCREEN_HEIGHT / 2), (SCREEN_WIDTH, SCREEN_HEIGHT / 2), 1)
+      pygame.draw.aaline(screen, (100, 0, 0), (SCREEN_WIDTH / 2, 0), (SCREEN_WIDTH / 2, SCREEN_HEIGHT), 1)
+      for p in points:
+        rect = pygame.Rect(p[0] - 5, p[1] - 5, 10, 10)
+        pygame.draw.rect(screen, (0, 0, 255), rect, 4)
+      if len(l1) > 1 and len(l2) > 1:
+        pygame.draw.aalines(screen, (0, 255, 0), False, l1, 1)
+        pygame.draw.aalines(screen, (255, 0, 0), False, l2, 1)
+      pygame.display.flip()
+      changed = False
 
 
 if __name__ == '__main__':
