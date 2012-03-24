85ab9fbe33fab264a2a520fd70faa68bd830cc46
berson_r <berson_r@epita.fr>
2012-03-24 18:40:11 +0100

    Now 'points' are displayed in blue where you cliked

--- approx.py
+++ approx.py
@@ -4,7 +4,12 @@ import functools
 SCREEN_WIDTH = 1640
 SCREEN_HEIGHT = 980
 
+
 def lagrange(points, x):
+  """
+    Using Lagrange method, interpol the points present in the 'points' list
+    and return the value of the polynom at point x.
+  """
 
   res = 0.0
 
@@ -25,6 +30,9 @@ def lagrange(points, x):
 
 
 def interpol(points):
+  """
+    Using the interpolation, build the set of points to draw
+  """
 
   result = []
   lagrange_compute = functools.partial(lagrange, points)
@@ -33,6 +41,7 @@ def interpol(points):
     result.append((i, lagrange_compute(i)))
 
   return result
+
 
 
 def main():
@@ -54,10 +63,13 @@ def main():
         running = 0
 
     screen.fill((0, 0, 0))
-    pygame.draw.line(screen, (100, 0, 0), (0, SCREEN_HEIGHT / 2), (SCREEN_WIDTH, SCREEN_HEIGHT / 2), 1)
-    pygame.draw.line(screen, (100, 0, 0), (SCREEN_WIDTH / 2, 0), (SCREEN_WIDTH / 2, SCREEN_HEIGHT), 1)
+    pygame.draw.aaline(screen, (100, 0, 0), (0, SCREEN_HEIGHT / 2), (SCREEN_WIDTH, SCREEN_HEIGHT / 2), 1)
+    pygame.draw.aaline(screen, (100, 0, 0), (SCREEN_WIDTH / 2, 0), (SCREEN_WIDTH / 2, SCREEN_HEIGHT), 1)
+    for p in points:
+      rect = pygame.Rect(p[0] - 5, p[1] - 5, 10, 10)
+      pygame.draw.rect(screen, (0, 0, 255), rect, 4)
     if len(points) > 1:
-      pygame.draw.lines(screen, (0, 255, 0), False, interpol(points), 1)
+      pygame.draw.aalines(screen, (0, 255, 0), False, interpol(points), 1)
     pygame.display.flip()
 
 
